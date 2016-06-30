9257e7a1a415452f54b8c27f54d8bae464ea2bab
Mauricio Lara Soberanis <mauricio9308@gmail.com>
2016-06-30 16:40:17 -0500

    Adding the recast base connection setup

--- index.js
+++ index.js
@@ -1,11 +1,75 @@
 #!/usr/bin/env node
 
-'use strict'
+'use strict';
 
-const Telegraf = require('telegraf')
+const Telegraf = require('telegraf');
+var TelegrafRecast = require('telegraf-recast');
 
-const telegraf = new Telegraf(process.env.BOT_TOKEN)
+var app = new Telegraf('261398004:AAGtd_vgo2SKKzRqDd821fHZFwvCLghjcEE');
+const recast = new TelegrafRecast('4de44c33da0fc597351e5d140fda9c84');
 
-//TODO: AI
+// Setting the recast handler
+app.use( recast.middleware() );
 
-telegraf.startPolling()
+//Shows the current user stack
+var currentUserStack = [];
+
+//Represents the steps that the given user should follow
+// for the application to work
+var stepsToPass = [];
+
+/**
+ * Handler for the greeting of the user
+ * */
+recast.onIntent('greeting', function * () {
+    /* print of the messages */
+    console.log('Intent of greeting');
+    console.log(this.state.recast.intent);              // first intent
+    console.log(this.state.recast.sentence);            // first sentence
+    console.log(this.state.recast.message);
+
+
+    // Get first room entity
+    var numberAnswer = this.state.recast.firstEntity('number');
+    if( numberAnswer ){
+        //You provided the number
+        this.reply('Number ' + numberAnswer);
+    }else{
+        this.reply('You should reply a number');
+    }
+});
+
+/**
+ * Handler for the number intent of the application
+ * */
+recast.onIntent('number-answer', function * () {
+
+    /* print of the messages */
+    console.log('Intent of number-answer');
+    console.log(this.state.recast.intent);              // first intent
+    console.log(this.state.recast.message);
+    console.log(this.message);
+
+    // Get first room entity
+    var numberAnswer = this.state.recast.firstEntity('number');
+    if( numberAnswer ){
+        //You provided the number
+        this.reply('Number ' + numberAnswer);
+    }else{
+        this.reply('You should reply a number');
+    }
+});
+
+
+/**
+ * Handler for the number intent of the application
+ * */
+recast.onMessage( function * () {
+    console.log('Message handling');
+    console.log(this.state.recast.intent);
+    console.log(this.message);
+    console.log('==== END OF MESSAGE HANDLING ====')
+});
+
+//We start the polling process for the application
+app.startPolling();
