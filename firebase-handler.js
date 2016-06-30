7cc1098af3e4f58d9af83168b1f4256241e83860
Antoine Rousseau <antoine@rousseau.im>
2016-06-30 23:41:00 +0000

    Setup of the process binding in Firebase

---
+++ firebase-handler.js
@@ -0,0 +1,85 @@
+'use strict'
+
+const firebase = require('firebase')
+
+/**
+ * Registers the current user in the telegram database
+ * */
+exports.registerUser = (userId, context ) => {
+  var ref = firebase.database().ref('/users/' + userId);
+  ref.on("value", function(snapshot) {
+    var values = {};
+    if (snapshot.val() != null) {
+      values = snapshot.val();
+    }
+
+    //We set the reference of the current process
+    var currentProcess = ( values.process ? values.process : 'default' );
+
+    /* object to be saved to firebase */
+    var values = {
+      id : userId,
+      client: 'telegram',
+      process: currentProcess
+    };
+
+    //We save the instance data in the DB
+    firebase.database().ref("/users/" + userId).set(values);
+
+    //We return the current process information
+    return currentProcess;
+  }, function (errorObject) {
+    //Printing the error object
+    console.log( errorObject );
+
+    context.reply('¡Oh no!, Hubo un error intenta de nuevo');
+  });
+}
+
+/**
+* Updates the process reference for the given user
+*/
+exports.updateStep = ( userId, process, context ) => {
+  var ref = firebase.database().ref('/users/' + userId);
+  ref.on("value", function(snapshot) {
+    var values = {};
+    if (snapshot.val() != null) {
+      values = snapshot.val();
+    }
+
+    /* we update the reference for the given step */
+    var values = {};
+    values.process = value;
+
+    firebase.database().ref('/users/' + userId).set( values );
+  }, function (errorObject) {
+    //Printing the error object
+    console.log( errorObject );
+
+    context.reply('¡Oh no!, Hubo un error intenta de nuevo');
+  });
+}
+
+/**
+* Updates the step information for the given case
+*/
+exports.updateStep = ( userId, stepName, value, context ) => {
+  var ref = firebase.database().ref('/users/' + userId + '/stateInfo/');
+  ref.on("value", function(snapshot) {
+    var values = {};
+    if (snapshot.val() != null) {
+      values = snapshot.val();
+    }
+
+    /* we update the reference for the given step */
+    var values = {};
+    values[ stepName ] = value;
+
+    firebase.database().ref('/users/' + userId + '/stateInfo').set( values );
+  }, function (errorObject) {
+    //Printing the error object
+    console.log( errorObject );
+
+    context.reply('¡Oh no!, Hubo un error intenta de nuevo');
+  });
+}
--- index.js
+++ index.js
@@ -5,13 +5,21 @@
 const https = require('https')
 const Telegraf = require('telegraf')
 const TelegrafRecast = require('telegraf-recast')
+const firebase = require('firebase')
+const firebaseHandler = require('./firebase-handler')
+
+/* init of the firebase instance */
+firebase.initializeApp({
+  serviceAccount: "Firebase.json",
+  databaseURL: "https://cpmx7-hackathon.firebaseio.com/"
+})
 
 const telegrafOptions = {
   telegram: {
-    agent: new https.Agent({
-      keepAlive: true, // ✨ Magic here!
-      keepAliveMsecs: 5000,
-    }),
+  agent: new https.Agent({
+    keepAlive: true,
+    keepAliveMsecs: 5000,
+  }),
   },
 }
 
@@ -21,10 +29,92 @@ telegraf.use(Telegraf.memorySession())
 const recast = new TelegrafRecast(process.env.RECAST_TOKEN)
 telegraf.use(recast.middleware())
 
-// respond to "I want the weather in Paris."
-recast.onIntent('weather', function * () {
-  const location = this.state.recast.firstEntity('location')
-  this.reply('Sunny in ' + location.raw)
-})
+//Handling libraries of the Telegraf library
+const Extra = Telegraf.Extra
+const Markup = Telegraf.Markup
+
+//Setting the current state and process to the given user
+var currentProcess = undefined;
+var currentState = undefined;
+
+/**
+ * Handler for the greeting of the user
+ * */
+recast.onIntent('greeting', function * () {
+  /* print of the messages */
+  console.log('Intent of greeting');
+
+  //We just simply reply the user answer
+  this.reply('¡Hola soy Luis tu asistente de construcción personal!' +
+  '\nPara empezar con dime. ¿Qué clase de proyecto quieres realizar? ');
+
+  //We give the chance to the user to select the process to be followed
+  this.reply('Keyboard wrap', Extra.markup(
+    Markup.keyboard(['Reparar techo', 'Construir un cuarto',
+    'Poner piso', 'Instalar loseta'], {columns: parseInt(ctx.match[1])})
+  ))
+});
+
+/**
+ * Handler for the process selection of the application
+ * */
+recast.onIntent('process-selection', function * () {
+  /* print of the messages */
+  console.log('Intent of process-selection');
+  console.log(this.state.recast.intent);              // first intent
+  console.log(this.state.recast.message);
+  console.log(this.message);
+
+  // Get first room entity
+  var process = this.state.recast.firstEntity('process');
+
+  if( process === 'Construir un cuarto'){
+    firebaseHandler.updateProcess( process );
+
+    //We then proceed to the next step in the build room process
+  }else{
+    this.reply('Por el momento no soportamos ese proceso :C');
+    //We give the chance to the user to select the process to be followed
+    this.reply('Keyboard wrap', Extra.markup(
+      Markup.keyboard(['Reparar techo', 'Construir un cuarto',
+      'Poner piso', 'Instalar loseta'], {columns: parseInt(ctx.match[1])})
+    ))
+  }
+});
+
+/**
+ * Handler for the number intent of the application
+ * */
+recast.onIntent('number-answer', function * () {
+  /* print of the messages */
+  console.log('Intent of number-answer');
+  console.log(this.state.recast.intent);              // first intent
+  console.log(this.state.recast.message);
+  console.log(this.message);
+
+  // Get first room entity
+  var numberAnswer = this.state.recast.firstEntity('number');
+  if( numberAnswer ){
+    //You provided the number
+    this.reply('Number ' + numberAnswer);
+  }else{
+    this.reply('You should reply a number');
+  }
+});
+
+
+/**
+ * Handler for the number intent of the application
+ * */
+recast.onMessage( function * () {
+  console.log('Message handling');
+  console.log('user id: '+ this.message.from.id );
+  console.log('==== END OF MESSAGE HANDLING ====');
+
+  // We try to register the user to the database if it's not set
+  currentProcess = firebaseHandler.registerUser( this.message.from.id, this /* context */);
+});
 
 telegraf.startPolling()
+
+console.log('Bot is listening')
